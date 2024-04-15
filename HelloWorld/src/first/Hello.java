93c4fe7aa43a4a063d29aa95052fc445d6fce740
dir-FY24iwasakikazuma <kazuma.iwasaki.y978@dirbato.co.jp>
2024-04-15 16:27:01 +0900

    20240415

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -5,6 +5,11 @@ public class Hello {
     public static void main(String[] args) {
    System.out.println("Hello, World!");
    System.out.println("こんにちは、世界！");
+   
+   for (int i = 0; i < 5; i++) {
+       System.out.println(i);
+   }
+  
     }
 
 }
